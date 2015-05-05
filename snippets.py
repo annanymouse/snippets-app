1ec9be2c619505e259a33573c7a6686ada8b2c1d
annanymouse <annachull@gmail.com>
2015-05-05 16:23:34 +0000

    Added stubs for a couple more functions to snippets.py

--- snippets.py
+++ snippets.py
@@ -1,14 +1,14 @@
 import logging
 
 #set the log output file, and the log level
-loggin.basicConfig(filename="snippets.log", level=logging.DEBUG)
+logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 
 def put(name, snippet):
     """
     Store a snippet with an associated name.
     Returns the name and the snippet.
     """
-    loggin.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
     return name, snippet
 
 def get(name):
@@ -16,6 +16,20 @@ def get(name):
     Retrieve the snippet with a given name.
     If there is no such snippet, return a message that snippet does not exist.
     Returns the snipppet
+    """
+    logging.error("FIXME: Unimplemented - get({!r})".format(name))
+    return ""
+
+def trash(name, snippet):
+    """
+    Trash a snippet with an associated name.
+    """
+    logging.error("FIXME: Unimplemented - get({!r})".format(name))
+    return "The message has been removed."
+
+def seek(name, snippet):
+    """
+    Returns all messages containing certain words.
     """
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     return ""
