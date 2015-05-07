dee34473f19b27dc25d3efd6a315006decb17203
annanymouse <annachull@gmail.com>
2015-05-07 02:19:17 +0000

    Finally got the search function to work.

--- snippets.py
+++ snippets.py
@@ -6,7 +6,7 @@ import logging
 import sys
 import psycopg2
 
-#set the log output file, and the log level
+#set the log output file, and the log level (this is the initialization)
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 
 logging.debug("Connecting to PostgreSQL")
@@ -42,37 +42,27 @@ def catalog():
     """Retrieve all the names(keywords) in the database."""
     logging.info("Getting all snippet names in the database...")
     with connection, connection.cursor() as cursor:
-        cursor.execute("select keyword from snippets")
+        cursor.execute("select keyword from snippets order by age")
         rows = cursor.fetchall()
     logging.debug("Snippet names retrieved successfully.")
-    #return "{}".format("\n".join([row for row in rows]))
-    return rows
-#     try:
-#         return "\n".join(["{}".format([row for row in rows])
-#     except TypeError:
-#         print("What's going on? There are no snippet names.")
-
-
-# def trash(name, snippet):
-#     """
-#     Trash a snippet with an associated name.
-#     """
-#     logging.error("FIXME: Unimplemented - get({!r})".format(name))
-#     return "The message has been removed."
-
-# def seek(name, snippet):
-#     """
-#     Returns all messages containing certain words.
-#     """
-#     logging.error("FIXME: Unimplemented - get({!r})".format(name))
-#     return ""
-
-# def update(name, snippet):
-#     """
-#     Updates the information in a snippet.
-#     """
-#     logging.error("FIXME: Unimplemented - get({!r})".format(name))
-#     return ""
+    try:
+        return rows
+    except TypeError:
+        print("What's going on? There are no snippet names.")
+        
+def search(name):
+    """Retrieve the snippet with a given name."""
+    logging.info("Getting all snippets with a keyword: {!r}".format(name))
+    with connection, connection.cursor() as cursor:
+        #key="%"+name+"%"
+        stmt = "select * from snippets where message like '%%%s%%'" % (name)
+        cursor.execute(stmt)
+        rows = cursor.fetchall()
+    logging.debug("Snippets retrieved successfully.")
+    try:
+        return rows
+    except TypeError:
+        print("There are no snippets with that string.")
 
 def main():
     """Main function"""
@@ -96,6 +86,11 @@ def main():
     logging.debug("Construcing catalog subparser")
     catalog_parser = subparsers.add_parser("catalog", help="Catalog of all snippet names")
     
+    #Subparser for the search command
+    logging.debug("Construcing search subparser")
+    search_parser = subparsers.add_parser("search", help="Search for a snippets containing a string.")
+    search_parser.add_argument("name", help="Name to search for in all the snippet messages.")
+    
     arguments = parser.parse_args(sys.argv[1:])
     # Convert parsed arguments from Namespace to dictionary
     arguments = vars(arguments)
@@ -112,6 +107,12 @@ def main():
         print("Here are all the snippet names:")
         # generator expression used inside join to print the results of the rows from cursor.fetchall() nicely
         print("{}".format('\n'.join(name[0] for name in names)))
-
+    elif command == "search":
+        results = search(**arguments)
+        print("Here are all the search results for that string:")
+        #print("{}".format('\n'.join((result[0] for result in results))))
+        for result in results:
+            print"{}:  {}".format(result[0], result[1])
+        
 if __name__=="__main__":
     main()
